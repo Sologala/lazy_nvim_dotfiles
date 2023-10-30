aed678f45020a74882951d092a68393e755d27fb
Sologala <dawsonwen123@gmail.com>
2023-10-30 14:28:20 +0800

    remove luasnip cmp fallback

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -34,8 +34,8 @@ return {
                         --     luasnip.expand()
                         -- elseif luasnip.expand_or_jumpable() then
                         --     luasnip.expand_or_jump()
-                        elseif check_backspace() then
-                            fallback()
+                        -- elseif check_backspace() then
+                        --     fallback()
                         else
                             fallback()
                         end
