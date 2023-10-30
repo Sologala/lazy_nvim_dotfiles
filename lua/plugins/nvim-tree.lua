faed20b967bbf46b98833680c934e255dc939067
Sologala <dawsonwen123@gmail.com>
2023-10-30 09:06:39 +0800

    D for toggle gitignore file in nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -26,6 +26,7 @@ return {
             api.config.mappings.default_on_attach(bufnr)
             vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Up'))
             vim.keymap.set('n', 'l', api.node.open.edit, opts('Help'))
+            vim.keymap.set('n', 'D', api.tree.toggle_gitignore_filter, opts('Toggle git ignore file'))
             -- custom mappings
             --vim.keymap.set('n', 'h', api.tree.close_node, opts('Close'))
             --vim.keymap.set('n', 'l', api.node.navigate.parent_close, opts('Close Parent'))
