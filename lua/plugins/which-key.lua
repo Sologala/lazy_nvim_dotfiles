16654b504215002e9dc5e4ecda812f9c68e2936f
WenDawson <dawsonwen123@gmail.com>
2024-01-22 23:08:45 -0800

    update which key for telescope some builtin functionality

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,22 +3,22 @@ return {
     config = function()
         local setups = {
             plugins = {
-                marks = false, -- shows a list of your marks on ' and `
-                registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
+                marks = false,        -- shows a list of your marks on ' and `
+                registers = true,     -- shows your registers on " in NORMAL or <C-r> in INSERT mode
                 spelling = {
-                    enabled = false, -- enabling this will show WhichKey when pressing z= to select spelling suggestions
+                    enabled = false,  -- enabling this will show WhichKey when pressing z= to select spelling suggestions
                     suggestions = 20, -- how many suggestions should be shown in the list?
                 },
                 -- the presets plugin, adds help for a bunch of default keybindings in Neovim
                 -- No actual key bindings are created
                 presets = {
-                    operators = false, -- adds help for operators like d, y, ... and registers them for motion / text object completion
-                    motions = false, -- adds help for motions
+                    operators = false,    -- adds help for operators like d, y, ... and registers them for motion / text object completion
+                    motions = false,      -- adds help for motions
                     text_objects = false, -- help for text objects triggered after entering an operator
-                    windows = true, -- default bindings on <c-w>
-                    nav = true, -- misc bindings to work with windows
-                    z = false, -- bindings for folds, spelling and others prefixed with z
-                    g = false, -- bindings for prefixed with g
+                    windows = true,       -- default bindings on <c-w>
+                    nav = true,           -- misc bindings to work with windows
+                    z = false,            -- bindings for folds, spelling and others prefixed with z
+                    g = false,            -- bindings for prefixed with g
                 },
             },
             -- add operators that will trigger motion and text object completion
@@ -38,25 +38,25 @@ return {
             },
             popup_mappings = {
                 scroll_down = "<c-d>", -- binding to scroll down inside the popup
-                scroll_up = "<c-u>", -- binding to scroll up inside the popup
+                scroll_up = "<c-u>",   -- binding to scroll up inside the popup
             },
             window = {
-                border = "rounded", -- none, single, double, shadow
-                position = "bottom", -- bottom, top
-                margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
+                border = "rounded",       -- none, single, double, shadow
+                position = "bottom",      -- bottom, top
+                margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
                 padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
                 winblend = 0,
             },
             layout = {
-                height = { min = 4, max = 25 },                                 -- min and max height of the columns
-                width = { min = 20, max = 50 },                                 -- min and max width of the columns
-                spacing = 3,                                                    -- spacing between columns
-                align = "left",                                                 -- align columns left, center or right
+                height = { min = 4, max = 25 },                                           -- min and max height of the columns
+                width = { min = 20, max = 50 },                                           -- min and max width of the columns
+                spacing = 3,                                                              -- spacing between columns
+                align = "left",                                                           -- align columns left, center or right
             },
-            ignore_missing = true,                                              -- enable this to hide mappings for which you didn't specify a label
+            ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
             hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-            show_help = true,                                                   -- show help message on the command line when the popup is visible
-            triggers = "auto",                                                  -- automatically setup triggers
+            show_help = true,                                                             -- show help message on the command line when the popup is visible
+            triggers = "auto",                                                            -- automatically setup triggers
             -- triggers = {"<leader>"} -- or specify a list manually
             triggers_blacklist = {
                 -- list of mode / prefixes that should never be hooked by WhichKey
@@ -64,7 +64,7 @@ return {
                 -- most people should not need to change this
                 i = { "j", "k" },
                 v = { "j", "k" },
-                n = { "e"},
+                n = { "e" },
             },
         }
         local opts = {
@@ -73,9 +73,9 @@ return {
             -- the prefix is prepended to every mapping part of `mappings`
             prefix = "<Space>",
             buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
-            silent = false,  -- use `silent` when creating keymaps
+            silent = false, -- use `silent` when creating keymaps
             noremap = true, -- use `noremap` when creating keymaps
-            nowait = false,  -- use `nowait` when creating keymaps
+            nowait = false, -- use `nowait` when creating keymaps
             expr = false,   -- use `expr` when creating keymaps
         }
 
@@ -163,6 +163,11 @@ return {
                 q = { "<cmd>Trouble quickfix<cr>", "Quick Fix" },
                 u = { "<cmd>Trouble lsp_references<cr>", "Usage" },
                 g = { "<cmd>Gitsigns setloclist<cr>", "Open changed hunk" },
+            },
+            t = {
+                c = { "<cmd> lua require('telescope.builtin').command_history()<cr>", "Command History" },
+                f = { "<cmd> lua require('telescope.builtin').oldfiles()<cr>", "OpenedFile History" },
+                s = { "<cmd> lua require('telescope.builtin').search_history()<cr>", "Search History" }
             },
 
             -- g = {
