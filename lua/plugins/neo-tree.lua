b40b61fa7f052b57dabf1a84a3ec63592557e2f4
WenDawson <dawsonwen123@gmail.com>
2024-03-10 10:58:48 +0800

    fix neotree by update to v3.20

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
+    tag = "3.20",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
