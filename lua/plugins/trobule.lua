6bf6b20bcee37f5f37cbed86607528bf35a2f5e8
WenDawson <dawsonwen123@gmail.com>
2024-03-26 14:56:34 +0800

    fix trouble jump and close mapping

--- lua/plugins/trobule.lua
+++ lua/plugins/trobule.lua
@@ -5,11 +5,14 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
+        --
         auto_close = true,
-        jump = { "<tab>", "<2-leftmouse>" },   -- jump to the diagnostic or open / close folds
-        open_split = { "<c-x>" },              -- open buffer in new split
-        open_vsplit = { "<c-v>" },             -- open buffer in new vsplit
-        open_tab = { "<c-t>" },                -- open buffer in new tab
-        jump_close = { "o", "<cr>" },          -- jump to the diagnostic and close the list
+        action_keys = {
+            jump = { "<tab>", "<2-leftmouse>" }, -- jump to the diagnostic or open / close folds
+            open_split = { "<c-x>" },        -- open buffer in new split
+            open_vsplit = { "<c-v>" },       -- open buffer in new vsplit
+            open_tab = { "<c-t>" },          -- open buffer in new tab
+            jump_close = { "<cr>", "o", },   -- jump to the diagnostic and close the list
+        }
     },
 }
