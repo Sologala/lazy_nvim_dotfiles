0cce506fe6fdadacea0dd9597ebf8ba52db894e1
WenDawson <dawsonwen123@gmail.com>
2024-03-26 14:53:36 +0800

    add trouble jump and close mapping

--- lua/plugins/trobule.lua
+++ lua/plugins/trobule.lua
@@ -1,9 +1,15 @@
 return {
- "folke/trouble.nvim",
- dependencies = { "nvim-tree/nvim-web-devicons" },
- opts = {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
- },
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        auto_close = true,
+        jump = { "<tab>", "<2-leftmouse>" },   -- jump to the diagnostic or open / close folds
+        open_split = { "<c-x>" },              -- open buffer in new split
+        open_vsplit = { "<c-v>" },             -- open buffer in new vsplit
+        open_tab = { "<c-t>" },                -- open buffer in new tab
+        jump_close = { "o", "<cr>" },          -- jump to the diagnostic and close the list
+    },
 }
