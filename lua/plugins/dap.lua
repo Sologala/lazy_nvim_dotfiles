272e832c49416e93b835cb14b895f959fc115565
WenDawson <dawsonwen123@gmail.com>
2024-08-19 11:05:59 +0800

    add dap relative plugin

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,105 @@
+return {
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = {"mfussenegger/nvim-dap"},
+        opts = {
+            ensure_installed = { "cppdbg" }
+        },
+        keys = {
+            {
+                '<F9>',
+                function()
+                    require'dap'.toggle_breakpoint()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            },
+            {
+                '<F5>',
+                function()
+                    if vim.fn.filereadable(".vscode/launch.json") then
+                        require("dap.ext.vscode").load_launchjs(nil, { cpptools = { "c", "cpp" } })
+                    end
+                    require'dap'.continue()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            },
+
+            {
+                '<F10>',
+                function()
+                    require'dap'.step_over()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            },
+            {
+                '<F11>',
+                function()
+                    require'dap'.step_into()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            },
+            {
+                '<F4>',
+                function()
+                    require'dap'.terminate()
+                    require("dapui").close()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            }, 
+            {
+                '<F3>',
+                function()
+                    require("dapui").toggle()
+                end,
+                mode = 'n',
+                desc = '[F]ormat buffer',
+            }, 
+        },
+
+    },
+    { 
+        "rcarriga/nvim-dap-ui", 
+        dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio", "jay-babu/mason-nvim-dap.nvim"},
+        config = function()
+            local dap, dapui = require("dap"), require("dapui")
+            dap.adapters.cppdbg = {
+                id = 'cppdbg',
+                type = 'executable',
+                 command = vim.fn.stdpath('data') .. '/mason/bin/OpenDebugAD7',
+                 args = {},
+                 attach = {
+                   pidProperty = "processId",
+                   pidSelect = "ask"
+                 },
+               }
+            require("dapui").setup()
+                dap.listeners.before.attach.dapui_config = function()
+              dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+              dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+              dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+              dapui.close()
+            end
+        end,
+        -- keys = {
+        --     {
+        --         '<F4>',
+        --         function()
+        --             require'dap'.step_into()
+        --         end,
+        --         mode = 'n',
+        --         desc = '[F]ormat buffer',
+        --     },
+        -- }
+    }
+}
