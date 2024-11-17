715e08dce11a612b46ed19ae8f38aac868db8086
WenDawson <dawsonwen123@gmail.com>
2024-11-17 11:02:46 +0800

    update telescope to 0.1.8 and use rg to find files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,134 +1,138 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.4',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        "nvim-telescope/telescope-live-grep-args.nvim"
-    },
-    config = function()
-        local telescope = require("telescope")
-        local actions = require("telescope.actions")
-        local lga_actions = require("telescope-live-grep-args.actions")
-        telescope.setup {
-            defaults = {
-                file_ignore_patterns = {
-                    "^build/",
-                    "^out/",
-                    "^devel/",
-                    "^install/",
-                    "^logs/",
-                    "^.cache/",
-                    "^.clangd/",
-                    "^.catkin_tools/",
-                    "^.cache/",
-                    "^.git/",
-                    "^3rdparty/"
-                },
-                prompt_prefix = " ",
-                selection_caret = " ",
-                path_display = {
-                    shorten = {
-                        -- e.g. for a path like
-                        --   `alpha/beta/gamma/delta.txt`
-                        -- setting `path_display.shorten = { len = 1, exclude = {1, -1} }`
-                        -- will give a path like:
-                        --   `alpha/b/g/delta.txt`
-                        len = 10,
-                        exclude = { 1, -1 }
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            "nvim-telescope/telescope-live-grep-args.nvim"
+        },
+        config = function()
+            local telescope = require("telescope")
+            local actions = require("telescope.actions")
+            local lga_actions = require("telescope-live-grep-args.actions")
+            telescope.setup {
+                defaults = {
+                    file_ignore_patterns = {
+                        "^build/",
+                        "^out/",
+                        "^devel/",
+                        "^install/",
+                        "^logs/",
+                        "^.cache/",
+                        "^.clangd/",
+                        "^.catkin_tools/",
+                        "^.cache/",
+                        "^.git/",
+                        "^3rdparty/"
                     },
-                },
-
-                mappings = {
-                    i = {
-                        ["<C-p>"] = actions.cycle_history_next,
-                        ["<C-n>"] = actions.cycle_history_prev,
-
-                        ["<C-j>"] = actions.move_selection_next,
-                        ["<C-k>"] = actions.move_selection_previous,
-
-                        ["<C-c>"] = actions.close,
-
-                        ["<Down>"] = actions.move_selection_next,
-                        ["<Up>"] = actions.move_selection_previous,
-
-                        ["<CR>"] = actions.select_default,
-                        ["<C-x>"] = actions.select_horizontal,
-                        ["<C-v>"] = actions.select_vertical,
-                        ["<C-t>"] = actions.select_tab,
-
-                        ["<C-u>"] = actions.preview_scrolling_up,
-                        ["<C-d>"] = actions.preview_scrolling_down,
-
-                        ["<PageUp>"] = actions.results_scrolling_up,
-                        ["<PageDown>"] = actions.results_scrolling_down,
-
-                        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-                        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-                        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-                        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-                        ["<C-l>"] = actions.complete_tag,
-                        ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
-                    },
-
-                    n = {
-                        ["<esc>"] = actions.close,
-                        ["<CR>"] = actions.select_default,
-                        ["<C-x>"] = actions.select_horizontal,
-                        ["<C-v>"] = actions.select_vertical,
-                        ["<C-t>"] = actions.select_tab,
-
-                        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
-                        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
-                        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
-                        ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-
-                        ["j"] = actions.move_selection_next,
-                        ["k"] = actions.move_selection_previous,
-                        ["H"] = actions.move_to_top,
-                        ["M"] = actions.move_to_middle,
-                        ["L"] = actions.move_to_bottom,
-
-                        ["<Down>"] = actions.move_selection_next,
-                        ["<Up>"] = actions.move_selection_previous,
-                        ["gg"] = actions.move_to_top,
-                        ["G"] = actions.move_to_bottom,
-
-                        ["<C-u>"] = actions.preview_scrolling_up,
-                        ["<C-d>"] = actions.preview_scrolling_down,
-
-                        ["<PageUp>"] = actions.results_scrolling_up,
-                        ["<PageDown>"] = actions.results_scrolling_down,
-
-                        ["?"] = actions.which_key,
-                    },
-                },
-            },
-            pickers = {
-                find_files = {
-                    theme = "dropdown",
-                    previewer = true,
-                    -- find_command = { "find", "-type", "f" },
-                    -- find_command = { "fd", "-H", "-I", "-t f", "--strip-cwd-prefix=always" }, -- "-H" search hidden files, "-I" do not respect to gitignore
-                    find_command = { "fd", "-H", "-I", "--type", "f"}, -- "-H" search hidden files, "-I" do not respect to gitignore
-                },
-            },
-            extensions = {
-                live_grep_args = {
-                    auto_quoting = true, -- enable/disable auto-quoting
-                    -- define mappings, e.g.
-                    mappings = {         -- extend mappings
-                        i = {
-                            ["<C-k>"] = lga_actions.quote_prompt(),
-                            ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                    prompt_prefix = " ",
+                    selection_caret = " ",
+                    path_display = {
+                        shorten = {
+                            -- e.g. for a path like
+                            --   `alpha/beta/gamma/delta.txt`
+                            -- setting `path_display.shorten = { len = 1, exclude = {1, -1} }`
+                            -- will give a path like:
+                            --   `alpha/b/g/delta.txt`
+                            len = 10,
+                            exclude = { 1, -1 }
                         },
                     },
-                    -- ... also accepts theme settings, for example:
-                    theme = "ivy", -- use dropdown theme
-                    -- theme = { }, -- use own theme spec
-                    -- layout_config = { mirror=true }, -- mirror preview pane
-                }
-            },
-        }
-        telescope.load_extension("live_grep_args")
-    end
+
+                    mappings = {
+                        i = {
+                            ["<C-p>"] = actions.cycle_history_next,
+                            ["<C-n>"] = actions.cycle_history_prev,
+
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+
+                            ["<C-c>"] = actions.close,
+
+                            ["<Down>"] = actions.move_selection_next,
+                            ["<Up>"] = actions.move_selection_previous,
+
+                            ["<CR>"] = actions.select_default,
+                            ["<C-x>"] = actions.select_horizontal,
+                            ["<C-v>"] = actions.select_vertical,
+                            ["<C-t>"] = actions.select_tab,
+
+                            ["<C-u>"] = actions.preview_scrolling_up,
+                            ["<C-d>"] = actions.preview_scrolling_down,
+
+                            ["<PageUp>"] = actions.results_scrolling_up,
+                            ["<PageDown>"] = actions.results_scrolling_down,
+
+                            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                            ["<C-l>"] = actions.complete_tag,
+                            ["<C-_>"] = actions.which_key, -- keys from pressing <C-/>
+                        },
+
+                        n = {
+                            ["<esc>"] = actions.close,
+                            ["<CR>"] = actions.select_default,
+                            ["<C-x>"] = actions.select_horizontal,
+                            ["<C-v>"] = actions.select_vertical,
+                            ["<C-t>"] = actions.select_tab,
+
+                            ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+                            ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+                            ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+                            ["<M-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+
+                            ["j"] = actions.move_selection_next,
+                            ["k"] = actions.move_selection_previous,
+                            ["H"] = actions.move_to_top,
+                            ["M"] = actions.move_to_middle,
+                            ["L"] = actions.move_to_bottom,
+
+                            ["<Down>"] = actions.move_selection_next,
+                            ["<Up>"] = actions.move_selection_previous,
+                            ["gg"] = actions.move_to_top,
+                            ["G"] = actions.move_to_bottom,
+
+                            ["<C-u>"] = actions.preview_scrolling_up,
+                            ["<C-d>"] = actions.preview_scrolling_down,
+
+                            ["<PageUp>"] = actions.results_scrolling_up,
+                            ["<PageDown>"] = actions.results_scrolling_down,
+
+                            ["?"] = actions.which_key,
+                        },
+                    },
+                },
+                pickers = {
+                    find_files = {
+                        theme = "dropdown",
+                        -- previewer = true,
+                        -- find_command = { "find", "-type", "f" },
+                        -- find_command = { "fd", "-H", "-I", "-t f", "--strip-cwd-prefix=always" }, -- "-H" search hidden files, "-I" do not respect to gitignore
+                        -- find_command = { "fd", "-H", "-I", "--type", "f" }, -- "-H" search hidden files, "-I" do not respect to gitignore
+                        find_command = { "rg", "--files", "--sort", "path" },
+                    },
+                },
+                extensions = {
+                    live_grep_args = {
+                        auto_quoting = true, -- enable/disable auto-quoting
+                        -- define mappings, e.g.
+                        mappings = {     -- extend mappings
+                            i = {
+                                ["<C-k>"] = lga_actions.quote_prompt(),
+                                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+                            },
+                        },
+                        -- ... also accepts theme settings, for example:
+                        theme = "ivy", -- use dropdown theme
+                        -- theme = { }, -- use own theme spec
+                        -- layout_config = { mirror=true }, -- mirror preview pane
+                    }
+                },
+            }
+            telescope.load_extension("live_grep_args")
+        end
+    },
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
 }
