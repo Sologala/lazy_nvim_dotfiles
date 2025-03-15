2c955b7d5e4edd265bf57c4fc98458d1d2a67311
WenDawson <dawsonwen123@gmail.com>
2025-03-15 23:06:57 +0800

    disable yazi

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -1,33 +1,33 @@
 return {
-    "mikavilpas/yazi.nvim",
-    event = "VeryLazy",
-    keys = {
-        -- 👇 in this section, choose your own keymappings!
-        -- {
-        --     "<leader>e",
-        --     "<cmd>Yazi<cr>",
-        --     desc = "Open yazi at the current file",
-        -- },
-        -- {
-        --     -- Open in the current working directory
-        --     "<leader>e",
-        --     "<cmd>Yazi cwd<cr>",
-        --     desc = "Open the file manager in nvim's working directory",
-        -- },
-        {
-            -- NOTE: this requires a version of yazi that includes
-            -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-            '<leader>e',
-            "<cmd>Yazi toggle<CR>",
-            desc = "Resume the last yazi session",
-        },
-    },
-    ---@type YaziConfig
-    opts = {
-        -- if you want to open yazi instead of netrw, see below for more info
-        open_for_directories = false,
-        keymaps = {
-            show_help = '<f1>',
-        },
-    },
+    -- "mikavilpas/yazi.nvim",
+    -- event = "VeryLazy",
+    -- keys = {
+    --     -- 👇 in this section, choose your own keymappings!
+    --     -- {
+    --     --     "<leader>e",
+    --     --     "<cmd>Yazi<cr>",
+    --     --     desc = "Open yazi at the current file",
+    --     -- },
+    --     -- {
+    --     --     -- Open in the current working directory
+    --     --     "<leader>e",
+    --     --     "<cmd>Yazi cwd<cr>",
+    --     --     desc = "Open the file manager in nvim's working directory",
+    --     -- },
+    --     {
+    --         -- NOTE: this requires a version of yazi that includes
+    --         -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+    --         '<leader>e',
+    --         "<cmd>Yazi toggle<CR>",
+    --         desc = "Resume the last yazi session",
+    --     },
+    -- },
+    -- ---@type YaziConfig
+    -- opts = {
+    --     -- if you want to open yazi instead of netrw, see below for more info
+    --     open_for_directories = false,
+    --     keymaps = {
+    --         show_help = '<f1>',
+    --     },
+    -- },
 }
